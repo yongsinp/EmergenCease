b55a1a7ce4b3ba3c62db6b2cfbd872a71e125a7f
Yongsin Park <yongsin.nlp@gmail.com>
2025-05-25 21:25:18 -0700

    Added get_time() for date and time extraction
     - Added get_time() function
     - Added code for downloading and loading spaCy model

--- src/ner/ner.py
+++ src/ner/ner.py
@@ -1,12 +1,25 @@
 import re
+import subprocess
+import sys
 
 import pandas as pd
-
-from src.utils.paths import DATA_DIR
+import spacy
 
 __all__ = ["get_urls"]
 
 REGEX_URL = re.compile(r"(?i)\b(?:https?:\/\/)?(?:www\.)[A-Za-z0-9.-]+\.[A-Za-z]{2,}(?:\/\S*)?\b")
+TIME_ENTITIES = {"DATE", "TIME"}
+KEYWORDS = {"until", "expiring"}
+
+# Download and load the spaCy model
+try:
+    MODEL_NAME = "en_core_web_sm"
+    nlp = spacy.load(MODEL_NAME)
+except OSError:
+    print(f"Model '{MODEL_NAME}' not found. Downloading...")
+    subprocess.check_call([sys.executable, "-m", "spacy", "download", MODEL_NAME])
+    nlp = spacy.load(MODEL_NAME)
+
 
 def get_urls(input: str) -> list[str]:
     """
@@ -24,3 +37,18 @@ def get_urls(input: str) -> list[str]:
     urls = REGEX_URL.findall(input)
     return urls if urls else []
 
+
+def get_time(input: str) -> str:
+    """
+    Extracts the expiration time from the input string.
+
+    Parameters:
+        input: The input string from which to extract the time.
+
+    Returns:
+        The time if found, otherwise an empty string.
+    """
+    if pd.isna(input) or not str(input).strip():  # Check for NaN and empty string
+        return []
+
+    return [ent for ent in nlp(input).ents if ent.label_ in TIME_ENTITIES]
