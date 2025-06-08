d9a158c2ec948f9fe1da821c3f2010bffdf689df
Yongsin Park <yongsin.nlp@gmail.com>
2025-06-08 02:00:31 -0700

    Added __all__ to paths.py

--- src/utils/paths.py
+++ src/utils/paths.py
@@ -2,6 +2,8 @@
 
 from pathlib import Path
 
+__all__ = ["PROJECT_ROOT", "DATA_DIR", "MODEL_DIR", "CONFIG_DIR"]
+
 PROJECT_ROOT = Path(__file__).parent.parent.parent
 DATA_DIR = PROJECT_ROOT / "data"
 MODEL_DIR = PROJECT_ROOT / "models"
