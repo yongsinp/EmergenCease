d78c2f5a049f786b6c2cd53bad71ac7787240f4d
Yongsin Park <yongsin.nlp@gmail.com>
2025-06-08 00:43:01 -0700

    Updated documentation for translate.py

--- src/cap_translator/translate.py
+++ src/cap_translator/translate.py
@@ -1,6 +1,5 @@
 import copy
 import os.path
-from logging import DEBUG
 from typing import Union, Optional
 
 import jsonschema.exceptions
@@ -14,46 +13,53 @@ from src.utils.paths import DATA_DIR, MODEL_DIR
 
 
 class Translator:
-    # Todo: Move path to a config file
-    TEMPLATES = read_json("./cap_templates.json")
+    """
+    A class to translate Common Alerting Protocol (CAP) data into multiple languages.
+    Events and languages supported by the FCC Wireless Emergency Alert Templates are translated using these templates and an LLM for information extraction.
+    The English info block serves as the source for information extraction by the LLM.
+    If the CAP data already includes information in a specific language, the original information is preserved.
+    """
+    TEMPLATES = read_json("./cap_templates.json")  # Todo: Move path to a config file
     LANGUAGES = {language for language in next(iter(TEMPLATES.values())).keys()}
 
     def __init__(self, model: str, adapter: Optional[str] = None) -> None:
         """
         Initializes the Translator with a specified LLM.
 
-        :param model:
+        Parameters:
+            model: A local path or Hugging Face model identifier for the model used in extraction.
+            adapter: Optional path to a model adapter.
         """
         self.model = model
         self.extractor = Extractor(model, adapter=adapter)
-        self.extractor.set_logger_level(DEBUG)
 
     @staticmethod
-    def _get_language(info_block: dict) -> str:
+    def _get_language(info_block: dict) -> Language:
         """
-        Extracts the language from the info block.
+        Extracts language information from the info block.
 
         Parameters:
-            info_block: A dictionary containing the info block.
+            info_block: The info block to extract from.
 
         Returns:
-            The language code, defaulting to 'en' if not specified.
+            The language code; defaults to 'en' if unspecified.
         """
         return info_block.get("language", Language.en.name).split("-")[0]
 
-    def _extract_info(self, en_info: list) -> dict:
+    def _extract_info(self, en_info: dict) -> dict:
         """
-        Extracts information from the provided list of info items.
+        Extracts relevant information from the English info block using the LLM.
 
         Parameters:
-            en_info: A list of dictionaries containing information items.
+            en_info: The English info block containing the alert texts.
 
         Returns:
-            A dictionary with extracted information.
+            A dictionary containing the extracted information, including event, expires, sender, url, and location.
         """
         if not en_info:
             return {}
 
+        # Extract alert texts to use for extraction
         headline = en_info.get("headline", "")
         description = en_info.get("description", "")
         instruction = en_info.get("instruction", "")
@@ -62,14 +68,17 @@ class Translator:
 
     def translate(self, source: Union[dict, Cap]) -> Cap:
         """
-        Translates the source data into multilingual CAP (Common Alerting Protocol) data.
+        Translates the source CAP data into a multilingual CAP (Common Alerting Protocol) data.
+        Only the events and languages supported by the FCC Wireless Emergency Alert Templates are translated.
+        Original information in the source is always prioritized.
 
         Parameters:
-            source: A dictionary or JSON string representing the source data.
+            source: A dictionary or a Cap object containing the CAP data to be translated. Dictionary should conform to the CAP schema.
 
         Returns:
-            A Cap object containing the translated data.
+            A Cap object containing the translated data. It maybe the same as the source if translation is not applicable.
         """
+        # Convert source to Cap object
         if isinstance(source, dict):
             try:
                 source = Cap.from_dict(source)
@@ -77,6 +86,7 @@ class Translator:
                 print(f"Validation error: {e.message}")
                 return source
 
+        # Get English info block
         info = source.content.get("info", [])
         en_info = next((item for item in info if Translator._get_language(item) == Language.en.name), None)
 
@@ -84,11 +94,12 @@ class Translator:
         if en_info is None:
             return source
 
+        # Extract required information from the English info block for preservation
         required_info = {key: None for key in SCHEMA['properties'].keys()}
-
         if event := EVENT_MAP.get(en_info.get("event", None), None):  # Default to Event.Other if not found
+            # Other is not supported by the FCC templates
             if event == Event.Other:
-                return source  # If the event is 'Other', return the source as is
+                return source
             required_info["event"] = event
         if expires := en_info.get("expires", None):
             required_info["expires"] = expires
@@ -99,6 +110,7 @@ class Translator:
         if location := [areaDesc for area in en_info.get("area", []) if (areaDesc := area.get("areaDesc", None))]:
             required_info["location"] = "; ".join(location)
 
+        # If any required information is missing, try extract it using the LLM
         if any(missing_info := [k for k, v in required_info.items() if v is None]):
             try:
                 extracted_info = self._extract_info(en_info)
@@ -106,12 +118,15 @@ class Translator:
                 print(f"Error extracting information: {e}")
                 return source
 
+            # Other is not supported by the FCC templates
             if extracted_info["event"] == Event.Other:
                 return source
 
+            # Update required_info with extracted information
             for key in missing_info:
                 required_info[key] = extracted_info[key]
 
+        # Add multilingual info blocks using the templates
         languages = set(Translator._get_language(item) for item in info)
         missing_languages = set(Translator.LANGUAGES) - languages
         for language in missing_languages:
@@ -135,7 +150,7 @@ class Translator:
 
 
 def main():
-    """Example code to demonstrate the usage of the Translator class."""
+    """Example code for using the Translator class."""
     model_path = os.path.join(MODEL_DIR, "Llama-3.2-1B-Instruct")
     adapter_path = os.path.join(MODEL_DIR, "LoRA-Llama-3.2-1B-Instruct")
     translator = Translator(model=model_path, adapter=adapter_path)
@@ -180,7 +195,7 @@ and protect yourself from flying debris.
 
     # Example usage with CAP data
     file_path = DATA_DIR / "IpawsArchivedAlerts.jsonl"
-    batch = next(read_jsonl_in_batches(file_path, batch_size=10))
+    batch = next(read_jsonl_in_batches(file_path, batch_size=1))
     for cap in batch:
         translated_cap = translator.translate(cap)
         print(translated_cap)
